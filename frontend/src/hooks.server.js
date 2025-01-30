d6894d154e30e3506880ca6ed4e253f2af10b3f8
lucabunino <66553738+lucabunino@users.noreply.github.com>
2025-01-30 10:47:41 +0100

    Font preload syntax

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -1,7 +1,8 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
 	const response = await resolve(event, {
-		preload: ({ type, path }) => type === 'js' | 'css' | 'font' || path.includes('/important/')
+		preload: ({ type, path }) => 
+			['js', 'css', 'font'].includes(type) || path.includes('/important/')
 	});
 
 	return response;
