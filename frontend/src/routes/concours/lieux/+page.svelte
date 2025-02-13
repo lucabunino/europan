5921d93d5cb1794f5077dccd16bfbca8dcc31fbf
lucabunino <66553738+lucabunino@users.noreply.github.com>
2025-02-13 17:12:39 +0100

    Golive

--- frontend/src/routes/concours/lieux/+page.svelte
+++ frontend/src/routes/concours/lieux/+page.svelte
@@ -4,23 +4,20 @@ $inspect(data)
 import { urlFor } from '$lib/utils/image';
 </script>
 
-<div class="content sites-grid">
-  {#each data.competition.featuredSites as featuredSite, i}
-  {$inspect(featuredSite)}
-  {#if featuredSite.siteUrl}
-  <a class="site no-hover" href={featuredSite.siteUrl} target="_blank" rel="noopener noreferrer">
-    {#if featuredSite.site.thumbnail}<img class="site-img" src={urlFor(featuredSite.site.thumbnail)} alt="Cover for {featuredSite.site.title}">{/if}
-    <h2 class="text-m">{featuredSite.site.title}</h2>
-  </a>
-  {:else}
-  <a class="site no-hover" href="lieux/{featuredSite.site.slug.current}">
-    {#if featuredSite.site.thumbnail}<img class="site-img" src={urlFor(featuredSite.site.thumbnail)} alt="Cover for {featuredSite.site.title}">{/if}
-    <h2 class="text-m">{featuredSite.site.title}</h2>
-  </a>
-  {/if}
-  {/each}
-</div>
-
-<style>
-
-</style>
+{#if data.competition.featuredSites}
+  <div class="content sites-grid">
+    {#each data.competition.featuredSites as featuredSite, i}
+    {#if featuredSite.siteUrl}
+      <a class="site no-hover" href={featuredSite.siteUrl} target="_blank" rel="noopener noreferrer">
+        {#if featuredSite.site.thumbnail}<img class="site-img" src={urlFor(featuredSite.site.thumbnail)} alt="Cover for {featuredSite.site.title}">{/if}
+        <h2 class="text-m">{featuredSite.site.title}</h2>
+      </a>
+    {:else}
+      <a class="site no-hover" href="lieux/{featuredSite.site.slug.current}">
+        {#if featuredSite.site.thumbnail}<img class="site-img" src={urlFor(featuredSite.site.thumbnail)} alt="Cover for {featuredSite.site.title}">{/if}
+        <h2 class="text-m">{featuredSite.site.title}</h2>
+      </a>
+    {/if}
+    {/each}
+  </div>
+{/if}
