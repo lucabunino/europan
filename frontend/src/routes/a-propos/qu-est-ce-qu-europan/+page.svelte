1d49c9ef12847b817f3e52138eff7ef9fba547c6
lucabunino <66553738+lucabunino@users.noreply.github.com>
2025-02-14 16:11:46 +0100

    boh

--- frontend/src/routes/a-propos/qu-est-ce-qu-europan/+page.svelte
+++ frontend/src/routes/a-propos/qu-est-ce-qu-europan/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 const { data } = $props()
 import { PortableText } from '@portabletext/svelte'
-import PortableTextStyle from '$lib/components/PortableTextStyle.svelte'
+import PortableTextStyle from '$lib/components/portableTextStyle.svelte'
 </script>
 
 <article class="content">
