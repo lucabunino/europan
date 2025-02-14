533bd21d9ce1b69dc777667ddb03408640569bbd
lucabunino <66553738+lucabunino@users.noreply.github.com>
2025-02-14 16:19:15 +0100

    Adapter Auto

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
